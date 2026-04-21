f079afbac576c4b53cd24a34483d9aff8e472ef5
Pol Corsín Inglesias <aircorsinpol@gmail.com>
2026-04-21 13:32:02 +0200

    Update EscaladorDAO.java

--- src/DAO/interfaces/EscaladorDAO.java
+++ src/DAO/interfaces/EscaladorDAO.java
@@ -1,5 +1,16 @@
 package DAO.interfaces;
 
-public class EscaladorDAO {
+import model.Escalador;
+import java.util.List;
+
+public interface EscaladorDAO {
+    // Operaciones CRUD básicas
+    void create(Escalador escalador);
+    void update(Escalador escalador);
+    void delete(int id);
     
+    // Búsquedas
+    Escalador findById(int id);
+    Escalador findByAlias(String alias);
+    List<Escalador> findAll();
 }
