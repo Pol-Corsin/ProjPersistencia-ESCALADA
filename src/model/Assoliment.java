e75b2a570d1dddaecd297331083ed7ed90b146a1
e-gutierrez2 <e.gutierrez2@sapalomera.cat>
2026-04-21 13:30:10 +0200

    Creacio de constructors getters i setters

--- src/model/Assoliment.java
+++ src/model/Assoliment.java
@@ -1,5 +1,55 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Assoliment {
+    private String escaladorAlias;
+    private int viaId;
+    private LocalDate dataCompletat;
+    private String grauAssolit;
+    
+    public Assoliment(String escaladorAlias, int viaId, LocalDate dataCompletat, String grauAssolit) {
+        this.escaladorAlias = escaladorAlias;
+        this.viaId = viaId;
+        this.dataCompletat = dataCompletat;
+        this.grauAssolit = grauAssolit;
+    }
+
+    // Alias del Escalador
+    public String getEscaladorAlias() {
+        return escaladorAlias;
+    }
+
+    public void setEscaladorAlias(String escaladorAlias) {
+        this.escaladorAlias = escaladorAlias;
+    }
+
+    // Id de la via
+    public int getViaId() {
+        return viaId;
+    }
+
+    public void setViaId(int viaId) {
+        this.viaId = viaId;
+    }
+
+    // Data de la completacio
+    public LocalDate getDataCompletat() {
+        return dataCompletat;
+    }
+
+    public void setDataCompletat(LocalDate dataCompletat) {
+        this.dataCompletat = dataCompletat;
+    }
+
+    // Grau de la via assolida
+    public String getGrauAssolit() {
+        return grauAssolit;
+    }
+
+    public void setGrauAssolit(String grauAssolit) {
+        this.grauAssolit = grauAssolit;
+    }
+
     
 }
--- src/model/Llarg.java
+++ src/model/Llarg.java
@@ -1,5 +1,53 @@
 package model;
 
 public class Llarg {
+    private int viaId;
+    private int numeroLlarg;
+    private int llargada;
+    private String grau;
+    
+    public Llarg(int viaId, int numeroLlarg, int llargada, String grau) {
+        this.viaId = viaId;
+        this.numeroLlarg = numeroLlarg;
+        this.llargada = llargada;
+        this.grau = grau;
+    }
+
+    // Id de la via
+    public int getViaId() {
+        return viaId;
+    }
+
+    public void setViaId(int viaId) {
+        this.viaId = viaId;
+    }
+    
+    //Cuants llargs te la via
+    public int getNumeroLlarg() {
+        return numeroLlarg;
+    }
+
+    public void setNumeroLlarg(int numeroLlarg) {
+        this.numeroLlarg = numeroLlarg;
+    }
+
+    // Llargada de la via
+    public int getLlargada() {
+        return llargada;
+    }
+
+    public void setLlargada(int llargada) {
+        this.llargada = llargada;
+    }
+
+    // El grau de dificultad de la via
+    public String getGrau() {
+        return grau;
+    }
+
+    public void setGrau(String grau) {
+        this.grau = grau;
+    }
+
     
 }
