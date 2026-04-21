f8b1a4ca8b86e3dfb17f43b88541aff556806713
e-gutierrez2 <e.gutierrez2@sapalomera.cat>
2026-04-21 14:00:37 +0200

    Menu

--- src/view/MenuTerminal.java
+++ src/view/MenuTerminal.java
@@ -1,5 +1,17 @@
 package view;
 
 public class MenuTerminal {
+    public static void missatge(String msg){
+        System.out.println(msg);
+    }
     
+    public static void menu(){
+        missatge("#####################");
+        missatge("#        MENU       #");
+        missatge("#####################");
+        missatge("1) Crear");
+        missatge("2) Listar");
+        missatge("3) Modificar");
+        missatge("4) Eliminar");
+    }
 }
