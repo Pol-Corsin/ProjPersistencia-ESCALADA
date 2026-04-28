0d9711bf44b71fb7c210701d51037dd0c4b400d3
Pol Corsín Inglesias <aircorsinpol@gmail.com>
2026-04-28 12:38:42 +0200

    Update SQLiteEscaladorDAO.java

--- src/DAO/sqlite/SQLiteEscaladorDAO.java
+++ src/DAO/sqlite/SQLiteEscaladorDAO.java
@@ -3,13 +3,23 @@ package DAO.sqlite;
 import DAO.interfaces.EscaladorDAO;
 import DAO.sqlite.escalador.*; // Importa todas tus clases de acción
 import model.Escalador;
+
+import java.sql.SQLException;
 import java.util.List;
 
 public class SQLiteEscaladorDAO implements EscaladorDAO {
 
     @Override
     public void create(Escalador escalador) {
-        new CreateEscalador().execute(escalador);
+        try {
+            new CreateEscalador().execute(escalador);
+        } catch (SQLException e) {
+            // Verificamos si el error es por violación de unicidad (Código 19 en SQLite)
+            if (e.getErrorCode() == 19) {
+                throw new RuntimeException("EL_ALIAS_YA_EXISTE");
+            }
+            throw new RuntimeException("ERROR_DESCONOCIDO");
+        }
     }
 
     @Override
