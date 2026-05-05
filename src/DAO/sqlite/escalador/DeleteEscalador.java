0a71cb3e84509eb7a03959df25339b89cb593b99
Paraca405 <aircorsinpol@gmail.com>
2026-05-05 12:48:36 +0200

    Update DeleteEscalador.java

--- src/DAO/sqlite/escalador/DeleteEscalador.java
+++ src/DAO/sqlite/escalador/DeleteEscalador.java
@@ -1,5 +1,23 @@
 package DAO.sqlite.escalador;
 
+import utils.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DeleteEscalador {
     
+    public void execute(int id) {
+        String sql = "DELETE FROM Escalador WHERE id = ?";
+
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, id);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("ERROR_ELIMINAR_ESCALADOR: " + e.getMessage());
+        }
+    }
 }
