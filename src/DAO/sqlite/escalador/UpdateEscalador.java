5aaa2672cd677271ceb16c613a9ff25c938ca1dd
Paraca405 <aircorsinpol@gmail.com>
2026-05-05 12:52:46 +0200

    Update update update UpdateEscalador

--- src/DAO/sqlite/escalador/UpdateEscalador.java
+++ src/DAO/sqlite/escalador/UpdateEscalador.java
@@ -1,5 +1,29 @@
 package DAO.sqlite.escalador;
 
+import model.Escalador;
+import utils.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class UpdateEscalador {
     
+    public void execute(Escalador escalador) {
+        String sql = "UPDATE Escalador SET alias = ?, nom = ?, edat = ?, estil_pref = ? WHERE id = ?";
+
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, escalador.getAlias());
+            pstmt.setString(2, escalador.getNom());
+            pstmt.setInt(3, escalador.getEdat());
+            pstmt.setString(4, escalador.getEstilPref());
+            pstmt.setInt(5, escalador.getId());
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("ERROR_MODIFICAR_ESCALADOR: " + e.getMessage());
+        }
+    }
 }
