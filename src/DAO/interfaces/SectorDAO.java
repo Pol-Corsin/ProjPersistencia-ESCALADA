81911d8972d2ebdacb10264242934b259fdb4b53
Pol Corsín Inglesias <aircorsinpol@gmail.com>
2026-05-06 11:08:51 +0200

    Interficie DAO de Sector

--- src/DAO/interfaces/SectorDAO.java
+++ src/DAO/interfaces/SectorDAO.java
@@ -1,5 +1,19 @@
 package DAO.interfaces;
 
-public class SectorDAO {
+import model.Sector;
+import java.util.List;
+
+public interface SectorDAO {
+    // Operaciones CRUD básicas
+    void create(Sector sector, int escolaId);
+    void update(Sector sector);
+    void delete(int id);
     
+    // Búsquedas
+    Sector findById(int id);
+    Sector findByNomAndEscolaId(String nom, int escolaId);
+    List<Sector> findAll();
+    List<Sector> findByEscolaId(int escolaId);
+    List<Sector> findAmbMesDeXVies(int x);
+    List<Sector> findByTipusVia(String tipusVia);
 }
