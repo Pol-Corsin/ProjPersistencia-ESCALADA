a16dd41c60887104932a215d592c32b3513f7d76
e-gutierrez2 <e.gutierrez2@sapalomera.cat>
2026-05-06 11:24:00 +0200

    Escola DAO acabado funciones de busqeda CRUD basicas y funciones extras de la poblacion

--- src/DAO/interfaces/EscolaDAO.java
+++ src/DAO/interfaces/EscolaDAO.java
@@ -1,5 +1,21 @@
 package DAO.interfaces;
 
-public class EscolaDAO {
-    
+import model.Escola;
+import java.util.List;
+
+public interface EscolaDAO {
+    // Operaciones CRUD Basicas
+    void create(Escola escola);
+    void update(Escola escola);
+    void delete(int id);
+
+    // Busquedas
+    Escola findById(int id);
+    Escola findByName(String nom);
+    List<Escola> findAll();
+
+    // Metodes Extres
+    void createPoblacio(int escolaId, String nomPoblacio);
+    void findPoblacioByName(int escolaId, String nomPolbacio);
+    void findPoblacioById(int escolaId, int poblacioId);
 }
