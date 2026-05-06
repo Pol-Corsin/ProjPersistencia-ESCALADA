76624ada23896755dae34a1811aa76043a8a6fcc
e-gutierrez2 <e.gutierrez2@sapalomera.cat>
2026-05-06 12:21:18 +0200

    Modificacions en l'apartat de viaDAO

--- src/DAO/interfaces/ViaDAO.java
+++ src/DAO/interfaces/ViaDAO.java
@@ -1,5 +1,28 @@
 package DAO.interfaces;
 
-public class ViaDAO {
+import model.Via;
+import java.util.List;
+public interface ViaDAO {
+    // CRUD
+    void create(Via via);
+    void update(Via via);
+    void delete(int id);
+
+    // Busquedas
+    Via findById(int id);
+    Via findByNom(String nomVia);
+    Via findByNomId(String nomVia, int id);
+    List<Via> findAll();
+    List<Via> findByPopularitat(String popularitat);
+    List<Via> findByAncoratge(String ancoratge);
+    List<Via> findByRoca(String roca);
+    List<Via> findByEstat(String estat);
+    List<Via> findByOrientacio(String orientacio);
+    List<Via> findByTipus(String tipus);
+
+    //Funciones Extras
+    void createLlarg(int viaId, int llarg);
+    void findByLlarg(int viaID, int llarg);
     
+
 }
