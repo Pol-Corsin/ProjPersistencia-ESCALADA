5d3732aa6e5bf43ed3d9e978069c184923835ddb
Paraca405 <aircorsinpol@gmail.com>
2026-05-10 21:12:45 +0200

    Update CreateAssoliment.java

--- src/DAO/sqlite/assoliment/CreateAssoliment.java
+++ src/DAO/sqlite/assoliment/CreateAssoliment.java
@@ -11,24 +11,26 @@ import java.sql.Statement;
 import java.time.LocalDate;
 
 public class CreateAssoliment {
-    
+
     public void execute(Assoliment assoliment) throws SQLException {
         String sql = "INSERT INTO Assoliments (escalador_alias, via_id, data_completat, grau_assolit) VALUES (?, ?, ?, ?)";
-        
+
         try (Connection conn = DBConnection.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-            
+                PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
             pstmt.setString(1, assoliment.getEscaladorAlias());
             pstmt.setInt(2, assoliment.getViaId());
-            pstmt.setString(3, assoliment.getDataCompletat() != null ? assoliment.getDataCompletat().toString() : LocalDate.now().toString());
+            pstmt.setString(3, assoliment.getDataCompletat() != null ? assoliment.getDataCompletat().toString()
+                    : LocalDate.now().toString());
             pstmt.setString(4, assoliment.getGrauAssolit());
-            
+
             pstmt.executeUpdate();
-            
-            // Obtenir l'ID generat pero taula Assoliments pot no tenir ID primari que es retorni
+
+            // Obtenir l'ID generat pero taula Assoliments pot no tenir ID primari que es
+            // retorni
             try (ResultSet generatedKeys = pstmt.getGeneratedKeys()) {
                 if (generatedKeys.next()) {
-                    // Si la taula no retorna ID, aquesta part no s'executarà
+                    // Si la taula no retorna ID, aquesta part no executa
                 }
             }
         }
