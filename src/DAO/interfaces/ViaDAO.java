34fde91ef15d0b975ede826c6bae6d3c99a66647
Paraca405 <aircorsinpol@gmail.com>
2026-05-10 21:13:18 +0200

    Update ViaDAO.java
    afegeix suport per llarg al viadao amb noves cerques i operacions crud. es canvien alguns metodes antics, sen treuen daltres i ara es poden gestionar millor els llargs de cada via.

--- src/DAO/interfaces/ViaDAO.java
+++ src/DAO/interfaces/ViaDAO.java
@@ -1,28 +1,40 @@
 package DAO.interfaces;
 
 import model.Via;
+import model.Llarg;
 import java.util.List;
+
 public interface ViaDAO {
-    // CRUD
+    // Operaciones CRUD básicas
     void create(Via via);
+
     void update(Via via);
+
     void delete(int id);
 
-    // Busquedas
+    // Búsquedas
     Via findById(int id);
-    Via findByNom(String nomVia);
-    Via findByNomId(String nomVia, int id);
+
     List<Via> findAll();
-    List<Via> findByPopularitat(String popularitat);
-    List<Via> findByAncoratge(String ancoratge);
-    List<Via> findByRoca(String roca);
+
+    List<Via> findBySectorId(int sectorId);
+
     List<Via> findByEstat(String estat);
-    List<Via> findByOrientacio(String orientacio);
-    List<Via> findByTipus(String tipus);
 
-    //Funciones Extras
-    void createLlarg(int viaId, int llarg);
-    void findByLlarg(int viaID, int llarg);
-    
+    List<Via> findByGrau(String grauMin, String grauMax);
 
+    List<Via> findByEscolaId(int escolaId);
+
+    List<Via> findRecentsApte(int dies);
+
+    List<Via> findMesLlargues(int escolaId, int limit);
+
+    // Métodos para Llarg
+    void createLlarg(Llarg llarg);
+
+    void updateLlarg(Llarg llarg);
+
+    void deleteLlarg(int id);
+
+    List<Llarg> findLlargsByViaId(int viaId);
 }
