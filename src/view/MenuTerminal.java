6ba2b4038e625ae54f63f7e293a2de0e3fd3d5df
Paraca405 <aircorsinpol@gmail.com>
2026-05-11 16:22:31 +0200

    afegeix un nou menu amb altres funcions i cerques utils sobre vies, escoles i escaladors. tambe arregla errors al text dels menus i corregeix algunes opcions que estaven mal posades

--- src/view/MenuTerminal.java
+++ src/view/MenuTerminal.java
@@ -18,7 +18,8 @@ public class MenuTerminal {
         missatge("2) Llistar");
         missatge("3) Modificar");
         missatge("4) Eliminar");
-        missatge("0) Soritr");
+        missatge("5) Altres funcions");
+        missatge("0) Sortir");
     }
 
     // Menu principal de crear de l'aplicació
@@ -30,7 +31,7 @@ public class MenuTerminal {
         missatge("2) VIA");
         missatge("3) Escola");
         missatge("4) Sector");
-        missatge("0) Enrere");
+        missatge("0) Sortir");
     }
 
     // Menu principal de modificar de l'aplicació
@@ -42,7 +43,7 @@ public class MenuTerminal {
         missatge("2) VIA");
         missatge("3) Escola");
         missatge("4) Sector");
-        missatge("0) Enrere");
+        missatge("0) Sortir");
     }
 
     // Menu principal de llistar de l'aplicació
@@ -54,7 +55,7 @@ public class MenuTerminal {
         missatge("2) VIA");
         missatge("3) Escola");
         missatge("4) Sector");
-        missatge("0) Enrere");
+        missatge("0) Sortir");
     }
 
     // Menu principal d'eliminar de l'aplicació
@@ -66,7 +67,23 @@ public class MenuTerminal {
         missatge("2) VIA");
         missatge("3) Escola");
         missatge("4) Sector");
-        missatge("0) Enrere");
+        missatge("0) Sortir");
+    }
+
+    // Menu d'altres funcions
+    public static void menuAltresFuncions() {
+        missatge("############################");
+        missatge("#     ALTRES FUNCIONS      #");
+        missatge("############################");
+        missatge("1) Vies disponibles d'una Escola");
+        missatge("2) Cercar vies per dificultat (rang)");
+        missatge("3) Cercar vies per estat");
+        missatge("4) Escoles amb restriccions actives");
+        missatge("5) Sectors amb més de X vies");
+        missatge("6) Escaladors mateix nivell màxim");
+        missatge("7) Vies recentment passades a Apte");
+        missatge("8) Vies més llargues d'una escola");
+        missatge("0) Sortir");
     }
 
     // #### FUNCIONES AUXILIARES (se usan en el controlador y queda mas limpio)
